969a6f1aecad8465f2bbcd252026275de0b4b3b8
chapinel <71669379+chapinel@users.noreply.github.com>
2022-03-07 14:51:57 -0800

    class typo

--- ui_service/patch-notes/pages/games/[game].js
+++ ui_service/patch-notes/pages/games/[game].js
@@ -172,7 +172,7 @@ export default function Game ({user, userData, notif, game, url, notes}) {
                 <button className={utilStyle.buttonSelect} onClick={() => setOpenDropdown(!openDropdown)}>
                   {selectText}
                   {openDropdown ? (<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-chevron-up"><polyline points="18 15 12 9 6 15"/></svg>) :(
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" stroke-linecap="round" strokeLinejoin="round" className="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"/></svg>)}
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"/></svg>)}
                 </button>
                 {openDropdown && (
                   <div className={utilStyle.selectDropdown}>
